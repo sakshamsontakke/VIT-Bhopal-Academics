f56c6556f408983c3c88269cd53c707eb981cac4
sakkkky <sontakkesaksham1510@gmail.com>
2026-09-21 15:29:43 +0530

    intial commit

--- 02_dataStructures/concateArrays.py
+++ 02_dataStructures/concateArrays.py
@@ -4,14 +4,14 @@ from array import*
 
 array1 = array("i" , [1,2,3])
 array2 = array("i" , [4,5,6])
-array3 = array("i" , [])
 
 n , j = len(array1) + len(array2) , 0
+array3 = array("i" , [0]*n)
 
 for i in range(n):
     if i < len(array1):
-        array3.append(array1[i])
+        array3[i] = array1[i]
     else:
-        array3.append(array2[j])
+        array3[i] = array2[j]
         j += 1    
 print(array3)            
