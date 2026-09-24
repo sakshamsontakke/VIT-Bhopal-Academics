6e4e06ad9128b07f1cd77b85b87e7b8aeee042a9
sakkkky <sontakkesaksham1510@gmail.com>
2026-09-24 11:01:08 +0530

    very diffcult question for gujju bhai(harshal)

--- 02_dataStructures/23_sumOfSeries.py
+++ 02_dataStructures/23_sumOfSeries.py
@@ -1,12 +1,10 @@
 # 1 + 121 + 12321 + 1234321 + 123454321
 
-n = int(input("Enter n: "))
-
-number = 0
-total = 0
-
-for i in range(1, n + 1):
-    number = number * 10 + i
-    total = total + number * number
-
-print("Sum =", total)
+n = int(input("enter a number: "))
+term = ""
+sum = 0
+for i in range(1 , n+1):
+    term = str(1)*i
+    sum = int(term)**2
+    print(term , sum)
+print(sum)
