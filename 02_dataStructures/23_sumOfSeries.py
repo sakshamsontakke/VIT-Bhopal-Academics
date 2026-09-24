5237b434241ca973bf4624539b67ca2769ba6331
sakkkky <sontakkesaksham1510@gmail.com>
2026-09-24 08:44:30 +0530

    intial commit

--- 02_dataStructures/23_sumOfSeries.py
+++ 02_dataStructures/23_sumOfSeries.py
@@ -1,5 +1,12 @@
 # 1 + 121 + 12321 + 1234321 + 123454321
 
+n = int(input("enter a number: "))
+temp  = ""
+sum = 0
+for i in range(1 , n+1):
+    for j in range(1 , len(temp)+3):
+        temp += str(i)
+    sum += int(temp)  
 
     
 
