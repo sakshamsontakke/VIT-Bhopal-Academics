594e93a21829a9122cf4947029fc094c2d311ac5
sakkkky <sontakkesaksham1510@gmail.com>
2026-09-24 11:14:48 +0530

    intial commit

--- 02_dataStructures/12_decimalToBinary.py
+++ 02_dataStructures/12_decimalToBinary.py
@@ -1,7 +1,7 @@
 # decimal to binary without using string function 
 
 # zero will not be there in the output if it's in the starting of the output that is the the reason we use string to perform binary opeartions
-
+n = 14
 decimal = 0
 rem = 0
 while n > 0:
